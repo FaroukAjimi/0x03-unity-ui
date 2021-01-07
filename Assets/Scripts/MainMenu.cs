7fcac60869917a7a2dbe9142465c5d87ebe22839
FaroukAjimi <benajimi@gmail.com>
2021-01-07 21:53:35 +0100

    Swooo

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,10 +8,18 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     public Button play;
     public Button quit;
     public void PlayMaze(){
         SceneManager.LoadScene("maze");
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
     }
     public void QuitMaze(){
         Debug.Log("Quit Game");
